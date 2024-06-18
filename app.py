106c70c38de13a89ed8a0c4c8e9aaf7334cdff52
rahmafaris981 <rahmafaris981@gmail.com>
2024-06-18 15:17:26 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<int:y>')
+@app.route('/<int:x>/<int:y>')
+@app.route('/<int:x>/<int:y>/<color1>/<color2>')
+def checkerboard(x=8, y=8, color1='black', color2='red'):
+    return render_template('index.html', x=x, y=y, color1=color1, color2=color2)
+
+if __name__ == '__main__':
+    app.run(debug=True)
